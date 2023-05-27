9ee935a6ed0354f793f822cde7a82143643d10f7
Yunthecloud <sulei.yin1@kmutt.ac.th>
2023-05-27 17:39:10 +0700

    backend edit post update

--- hackathon_backend/editPost.js
+++ hackathon_backend/editPost.js
@@ -2,8 +2,9 @@ module.exports = (req, res) => {
    const postId=req.body.id;
    const title=req.body.title;
    const description=req.body.description;
+   const visibility=req.body.visibility;
 
-    connection.query("UPDATE posts SET title=?,description=? WHERE id = ?", [title,description,postId], (err, rows) => {
+    connection.query("UPDATE posts SET title=?,description=?,visibility=? WHERE id = ?", [title,description,visibility,postId], (err, rows) => {
         if(err) {
             return res.json({
                 success: false,
